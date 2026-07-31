fbfa176478bc436b937fa0609ccfa99ff2df7acb
= <imrangconnect@gmail.com>
2026-07-31 10:24:56 +0400

    fix(customers): use Session::roleId() instead of unreliable global \ in page_header

--- dashboard/admin_elements/page_header_customer.php
+++ dashboard/admin_elements/page_header_customer.php
@@ -47,7 +47,7 @@ $created_by     = getTableAttr('created_by', DB::CUSTOMERS, $customer_id);
         </div>
         <div class="my-1 ms-auto d-flex align-items-center gap-2 flex-wrap">
             <a href="listing_<?php echo $module; ?>.php" class="btn btn-light btn-sm">Cancel</a>
-            <?php $can_approve = Roles::hasFullAccess($session_role_id) || Roles::isAccounts($session_role_id); ?>
+            <?php $can_approve = Roles::hasFullAccess(\App\Core\Session::roleId()) || Roles::isAccounts(\App\Core\Session::roleId()); ?>
             <?php if (granted_('edit', 'customers') && $can_approve) { ?>
                 <?php if ($approved == 0) { ?>
                     <a href="customer_overview.php?action=approved&customer_id=<?php echo $customer_id; ?>&approve=1" class="btn btn-success btn-sm"><i class="ph-check-circle me-1"></i> Approve</a>
